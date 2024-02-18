3c1deab2145edc4c7e2a3d92e7c1f1e0bf868093
qdenys <khrapov.denys.js@gmail.com>
2024-02-18 19:19:26 +0200

    update Homework 21

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', function () {
+	// Перший варіант
 	function slider() {
 		const btnPrev = document.querySelector('.btn-prev');
 		const btnNext = document.querySelector('.btn-next');
@@ -13,25 +14,25 @@ document.addEventListener('DOMContentLoaded', function () {
 				slideImage[currentIndex].classList.add('active');
 				currentIndex++;
 			}
-			if (currentIndex === slideImage.length){
+			if (currentIndex === slideImage.length) {
 				btnNext.classList.add('disabled')
 			}
-			if(currentIndex > 1){
+			if (currentIndex > 1) {
 				btnPrev.classList.remove('disabled');
 			}
 		});
 
-		btnPrev.addEventListener('click', ()=> {
-			if(currentIndex > 1){
+		btnPrev.addEventListener('click', () => {
+			if (currentIndex > 1) {
 				currentIndex--;
 				slideImage[currentIndex].classList.remove('active');
 				slideImage[currentIndex - 1].classList.add('active');
 			}
 
-			if(currentIndex < slideImage.length){
+			if (currentIndex < slideImage.length) {
 				btnNext.classList.remove('disabled');
 			}
-			if(currentIndex === 1){
+			if (currentIndex === 1) {
 				btnPrev.classList.add('disabled');
 			}
 
@@ -40,4 +41,39 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	slider();
+
+	// Другий варіант
+	const arrImages = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+
+	function newSlider(imagesArr) {
+		const buttonPrev = document.querySelector('.button-prev');
+		const buttonNext = document.querySelector('.button-next');
+		let image = document.querySelector('#myImg');
+		let currentIndex = 0;
+
+		function slideUpdate() {
+			image.src = `img/${imagesArr[currentIndex]}`;
+			buttonPrev.classList.toggle('disabled', currentIndex === 0);
+			buttonNext.classList.toggle('disabled', currentIndex === imagesArr.length - 1);
+		}
+
+		buttonNext.addEventListener('click', () => {
+			if (currentIndex < imagesArr.length) {
+				currentIndex++;
+			}
+			slideUpdate();
+		});
+		buttonPrev.addEventListener('click', () => {
+			if (currentIndex > 0) {
+				currentIndex--;
+			}
+			slideUpdate();
+		});
+		slideUpdate();
+
+	}
+
+	newSlider(arrImages);
+
+
 });
