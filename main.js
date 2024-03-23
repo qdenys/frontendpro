827a95193b8ebf2130f5d9d3805d554abf451462
qdenys <khrapov.denys.js@gmail.com>
2024-03-23 22:38:52 +0200

    Homework 30

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+	const array = [1, 2, [1.1, 1.2, 1.3], 3];
+	document.querySelector('.wrapper').innerHTML = generateList(array);
+
+	function generateList(array) {
+		let list = '<ul>';
+		array.forEach(item => {
+			if(Array.isArray(item)){
+				list += `<li>${generateSubList(item)}</li>`;
+
+			}else{
+				list += `<li>${item}</li>`;
+			}
+		})
+		list += '</ul>';
+		return list;
+	}
+
+
+	function generateSubList(array){
+		console.log(array)
+			let sublist = '<ul>';
+		array.forEach(item => {
+				sublist += `<li>${item}</li>`;
+			});
+		sublist += '</ul>';
+		return sublist;
+	}
+
+});
